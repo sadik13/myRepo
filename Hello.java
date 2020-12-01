a7e0b37045ed3be1db458a8ffdc077140f7d3370
sadik13 <74621438+sadik13@users.noreply.github.com>
2020-12-01 18:20:33 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World! updated by syed"); 
+        System.out.println("Welcome to java world, This is my first jenkin based java program"); 
     }
 }
