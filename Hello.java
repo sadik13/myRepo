9b0f6f23b2d0d6adc59df5f613d33304bfe5facc
sadik13 <74621438+sadik13@users.noreply.github.com>
2020-12-03 18:15:53 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Welcome to java world, This is my first jenkin based java program"); 
+        System.out.println("running this java in windows slave node");
     }
 }
